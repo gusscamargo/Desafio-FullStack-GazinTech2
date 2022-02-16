ec4167601961b8c162d23506bf93acb48c729bb6
gusscamargo <guga.power@hotmail.com>
2022-02-15 21:57:16 -0300

    Adicionando rotas

---
+++ src/backend/routes/nivel.ts
@@ -0,0 +1,14 @@
+import { Router } from "restify-router"
+const router = new Router()
+
+router.get("/")
+
+router.get("/:id")
+
+router.post("/add")
+
+router.put("/edit")
+
+router.del("/delete")
+
+export default router
